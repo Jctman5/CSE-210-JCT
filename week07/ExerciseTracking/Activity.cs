e1cf84f729b565f17324ff523242ad6efe4923ad
Jctman5 <Jctman5@gmail.com>
2025-08-14 17:08:23 -0400

    week 7

---
+++ week07/ExerciseTracking/Activity.cs
@@ -0,0 +1,29 @@
+using System;
+
+namespace ExerciseTracking
+{
+    abstract class Activity
+    {
+        private DateTime _date;
+        private int _minutes;
+
+        public Activity(DateTime date, int minutes)
+        {
+            _date = date;
+            _minutes = minutes;
+        }
+
+        public DateTime Date => _date;
+        public int Minutes => _minutes;
+
+        public abstract double GetDistance(); // in kilometers
+        public abstract double GetSpeed();    // in km/h
+        public abstract double GetPace();     // in minutes per km
+
+        public virtual string GetSummary()
+        {
+            return $"{_date:dd MMM yyyy} {this.GetType().Name} ({_minutes} min): " +
+                   $"Distance: {GetDistance():0.0} km, Speed: {GetSpeed():0.0} kph, Pace: {GetPace():0.00} min per km";
+        }
+    }
+}
--- week07/ExerciseTracking/Program.cs
+++ week07/ExerciseTracking/Program.cs
@@ -1,9 +1,23 @@
 using System;
+using System.Collections.Generic;
 
-class Program
+namespace ExerciseTracking
 {
-    static void Main(string[] args)
+    class Program
     {
-        Console.WriteLine("Hello World! This is the ExerciseTracking Project.");
+        static void Main(string[] args)
+        {
+            List<Activity> activities = new List<Activity>
+            {
+                new Running(new DateTime(2022, 11, 3), 30, 4.8),
+                new Cycling(new DateTime(2022, 11, 3), 45, 20.0),
+                new Swimming(new DateTime(2022, 11, 3), 40, 30)
+            };
+
+            foreach (var activity in activities)
+            {
+                Console.WriteLine(activity.GetSummary());
+            }
+        }
     }
 }
